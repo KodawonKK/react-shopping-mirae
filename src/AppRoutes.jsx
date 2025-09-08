57985efc9ff2d95d317d6d5cecd06c53494e62b1
KodawonKK <rhekdnjs0169@gmail.com>
2025-09-08 16:49:35 +0900

    feat : approute 파일 수정

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,12 +3,22 @@ import { useRoutes } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import AllProductPage from "./pages/AllProduct/AllProductPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 export const AppRoutes = () => {
   const routes = useRoutes([
-    { path: "/", element: <AppLayout /> },
-    { path: "/product:id", element: <ProductDetail /> },
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "all", element: <AllProductPage /> },
+        { path: "product/:id", element: <ProductDetail /> }
+      ]
+    },
     { path: "*", element: <NotFoundPage /> }
   ]);
+
   return routes;
 };
