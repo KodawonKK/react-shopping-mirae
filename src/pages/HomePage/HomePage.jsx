69f73bc3da8147bb4aa7a823060c260f93fca631
KodawonKK <rhekdnjs0169@gmail.com>
2025-09-09 16:24:39 +0900

    feat: 배너 슬라이더 추가

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+import banner1 from "../../assets/banner/banner_test.jpg";
+// import banner2 from "../../assets/banner/banner2.jpg";
+import banner2 from "../../assets/banner/banner3.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+
+const HomePage = () => {
+  const bannerList = [
+    {
+      img: banner1,
+      title: "Season Sale",
+      subtitle: "Up to 50% Off"
+    },
+    {
+      img: banner2,
+      title: "Wear the Tomorrow",
+      subtitle: ""
+    }
+  ];
+  return (
+    <div>
+      <div className={styles.bannerWrap}>
+        <Swiper
+          slidesPerView={1}
+          // modules={[Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false
+          }}
+        >
+          {bannerList.map((item, idx) => (
+            <SwiperSlide key={idx} className={styles.bannerImgWrap}>
+              <img src={item.img} alt="배너" />
+              <div className={styles.commentWrap}>
+                <h3 className={styles.comment}>{item.title}</h3>
+                {item.subtitle && <p>{item.subtitle}</p>}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
